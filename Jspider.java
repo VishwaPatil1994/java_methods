536bb550fbbe21aea3677f931c2f2956ad9ad966
vishwanath <vishwa08patil@gmail.com>
2023-10-01 13:56:34 +0530

    without arguments and without return value

---
+++ Jspider.java
@@ -0,0 +1,13 @@
+public class Jspider {
+  void display() // without arguments and without return value
+
+  {
+    System.out.println("Hai Jspider");
+    System.out.println("Bye Jspider");
+  }
+
+  public static void main(String[] args) {
+    Jspider j = new Jspider();
+    j.display();
+  }
+}
