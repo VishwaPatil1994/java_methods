2109ad4c7b8661b67fb19430b3eea4cf0e49a37c
vishwanath <vishwa08patil@gmail.com>
2023-10-01 14:02:03 +0530

    without arguments and without return type

---
+++ Maths.java
@@ -0,0 +1,14 @@
+public class Maths {
+  void add() // without arguments and without return type
+  {
+    int a = 2;
+    int b = 3;
+    System.out.println("sum: " + (a + b));
+  }
+
+  public static void main(String[] args) {
+    Maths m = new Maths();
+    m.add();
+  }
+
+}
